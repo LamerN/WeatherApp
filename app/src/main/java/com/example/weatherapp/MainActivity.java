4213a10fcc638eefceafd9648b13fb83204d7f80
LamerN <90185479+LamerN@users.noreply.github.com>
2022-08-28 01:24:43 +0300

    start main activity

--- app/src/main/java/com/example/weatherapp/MainActivity.java
+++ app/src/main/java/com/example/weatherapp/MainActivity.java
@@ -3,12 +3,36 @@ package com.example.weatherapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText editTextCity;
+    private Button buttonSearch;
+    private TextView textViewWeather;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editTextCity = findViewById(R.id.editTextCity);
+        buttonSearch = findViewById(R.id.buttonSearch);
+        textViewWeather = findViewById(R.id.textViewWeather);
+
+        buttonSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String cityName = editTextCity.getText().toString();
+                if ((cityName.trim().equals(""))) {
+                    Toast.makeText(MainActivity.this,R.string.toast_text, Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
     }
 }
